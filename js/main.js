1d75b73d9dcc5cccd4b4c5a8f80d0a4e5cb6dc26
Glib Shemenkov <shemenkovglib@gmail.com>
2023-07-07 12:51:28 +0200

    feat: add tel and video functional

--- js/main.js
+++ js/main.js
@@ -8,3 +8,20 @@ navBtn.onclick = function() {
 	nav.classList.toggle('header__top-row--mobile')
 	document.body.classList.toggle('no-scroll')
 }
+
+// phone Mask
+mask('[date-tel-input]');
+const phoneInputs = document.querySelectorAll('[date-tel-input]')
+
+phoneInputs.forEach(input => {
+	input.addEventListener('input', ()=>{
+		if(input.value === '+'){
+			input.value = '';
+		}
+		input.addEventListener('blur', ()=> {
+			if(input.value === '+'){
+				input.value = '';
+			}
+		})
+	})
+})
